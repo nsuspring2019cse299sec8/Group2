20068ea1939f2bfbdebc94cda0662bb069c57df5
nayemul Islam <nayemul.majumder@northsouth.edu>
2019-04-07 03:00:32 +0600

    Create connection.php
    database connection

---
+++ Code/lib/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$host="localhost";
+$user="root";
+$pass="";
+$db="hospital";
+
+$conn=new mysqli($host,$user,$pass,$db);
+
+    if($conn->connect_error){
+        die($conn->error);
+    }
+    else{
+         //echo "database connected";
+    }
+    
+
+?>
